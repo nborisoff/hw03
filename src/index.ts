4ab8c7bf27c9ecbd0a77f1e6e282cbeb6f387c82
nborisoff <nborissov256@gmail.com>
2024-04-08 22:56:28 +0300

    #3 feat: реализация API с mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { SETTINGS } from "./app/settings";
-import { app } from "./app/app";
 import { connectToDB } from "./db/mongo-db";
+import cors from "cors";
+import {blogRouter} from "./controllers/blogs/routes";
+import {postRouter} from "./controllers/posts/routes";
+
+
+export const app = express();
 
 const start = async () => {
   app.get("/", (req: Request, res: Response) => {
@@ -13,6 +18,12 @@ const start = async () => {
     process.exit(1);
     return;
   }
+  const parseBodyMiddleware = express.json();
+  app.use(parseBodyMiddleware);
+  app.use(cors());
+
+  app.use(SETTINGS.PATH.BLOGS, blogRouter);
+  app.use(SETTINGS.PATH.POSTS, postRouter);
 
   app.listen(SETTINGS.PORT, () => {
     console.log(`App listening on port ${SETTINGS.PORT}`);
