76962ac91e5af278c429e09bd8473837fdd4ce90
nborisoff <nborissov256@gmail.com>
2024-04-08 22:51:55 +0300

    #3 feat: реализация API с mongodb

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import { SETTINGS } from "./app/settings";
 import { app } from "./app/app";
 import { connectToDB } from "./db/mongo-db";
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello Samurai!");
-});
-
 const start = async () => {
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Hello Samurai!");
+  });
+
   if (!(await connectToDB())) {
     console.log("stop");
     process.exit(1);
