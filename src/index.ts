ef78d20c9566c05bb3a0af069a2a0f251866a6de
nborisoff <nborissov256@gmail.com>
2024-04-08 22:19:36 +0300

    #3 feat: реализация API с mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { SETTINGS } from "./app/settings";
-import { app } from "./app/app";
 import { connectToDB } from "./db/mongo-db";
+import cors from "cors";
+import { blogRouter } from "./controllers/blogs/routes";
+import { postRouter } from "./controllers/posts/routes";
+
+export const app = express();
+
+const parseBodyMiddleware = express.json();
+app.use(parseBodyMiddleware);
+app.use(cors());
+
+app.use(SETTINGS.PATH.BLOGS, blogRouter);
+app.use(SETTINGS.PATH.POSTS, postRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Samurai!");
